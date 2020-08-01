e995d79fe04486f3980fda0d26881c2abfb8f4cd
delvaux1986 <delvaux.robby@gmail.com>
2020-08-01 20:54:49 +0200

    nomenclature

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// VARIABLES CANVAS
+let can = document.getElementById('can');
+let ctx = can.getContext ;
+// VARIABLE BUTTON
+let start = document.getElementById('start');
+let reset = document.getElementById('reset');
