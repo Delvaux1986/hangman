9228bf44a02ea22c9ffc01c25748bc34e9aa3961
Delvaux1986 <delvaux.robby@gmail.com>
2020-08-05 22:12:43 +0200

    remove canvas 2th time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-// VARIABLES CANVAS
-let can = document.getElementById('can');
-let ctx = can.getContext ;
 // VARIABLE BUTTON
 let start = document.getElementById('start');
 let stoplight = document.getElementById('wordSpotLight');
